5968502aedd2776ee2c7b50730abba21e7950bff
peternu2020 <39267532+peternu2020@users.noreply.github.com>
2019-12-06 13:02:16 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/database';
 
-
+/*
 var firebaseConfig = {
     apiKey: "AIzaSyAOtUcwusxESbhbnpKXpUo1tnWRhSL38oA",
     authDomain: "getschooled-ea6b2.firebaseapp.com",
@@ -11,6 +11,16 @@ var firebaseConfig = {
     messagingSenderId: "81250038579",
     appId: "1:81250038579:web:d758e041ac7ac4459e0446"
 };
+*/
+var firebaseConfig = {
+    apiKey: "AIzaSyBE5si_VYqEAaVIFx1zX5de282ynxRV5Ik",
+    authDomain: "get-schooled-3a0f3.firebaseapp.com",
+    databaseURL: "https://get-schooled-3a0f3.firebaseio.com",
+    projectId: "get-schooled-3a0f3",
+    storageBucket: "get-schooled-3a0f3.appspot.com",
+    messagingSenderId: "5757094876",
+    appId: "1:5757094876:web:fc337ac772c7f67039dd19",
+  };
 
 const firebaseApp = app.initializeApp(firebaseConfig);
 
